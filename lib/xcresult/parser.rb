bcb9573cf068db4dab9d5b15b7b219073704a5f0
Sarad Pant <saradr.pant@gmail.com>
2026-01-28 11:02:50 -0500

    Use File.expand_path for output paths

--- lib/xcresult/parser.rb
+++ lib/xcresult/parser.rb
@@ -47,7 +47,7 @@ module XCResult
 
       # Exports all xccovreport files from the report references
       ids.each_with_index.map do |id, i|
-        output_path = File.join(destination, "action_#{i}.xccovreport")
+        output_path = File.expand_path(File.join(destination, "action_#{i}.xccovreport"))
         cmd = xcresulttool_command("export", "--path #{path} --id '#{id}' --output-path #{output_path} --type file")
         execute_cmd(cmd)
 
@@ -66,7 +66,7 @@ module XCResult
 
       # Exports all xcovarchive directories from the archive references
       ids.each_with_index.map do |id, i|
-        output_path = File.join(destination, "action_#{i}.xccovarchive")
+        output_path = File.expand_path(File.join(destination, "action_#{i}.xccovarchive"))
         cmd = xcresulttool_command("export", "--path #{path} --id '#{id}' --output-path #{output_path} --type directory")
         execute_cmd(cmd)
 
